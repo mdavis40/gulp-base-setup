9a8aa36868cb8687f49050cb9c7cfbbcfa082ebd
mdskd <mdavis0816@gmail.com>
2015-09-04 21:44:45 -0700

    wire up nodemon to restart expressjs server. make browser-sync work nicely.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,23 @@ var gulp = require('gulp'),
     minifycss = require('gulp-minify-css'),
     minifyjs = require('gulp-uglify'),
     browserSync = require('browser-sync'),
-    concat = require('gulp-concat')
+    concat = require('gulp-concat'),
+    nodemon = require('gulp-nodemon')
+
+gulp.task('nodemon', function(cb) {
+  var started = false
+
+  return nodemon({
+    script: 'server.js',
+    watch: 'server.js'
+  }).on('start', function() {
+    if (started) {
+      return
+    }
+    cb()
+    started = true
+  })
+})
 
 gulp.task('html', function() {
   return gulp.src('app/index.html')
@@ -36,16 +52,21 @@ gulp.task('js', function() {
     }))
 })
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', [ 'nodemon' ], function() {
   browserSync({
-    server: {
-      baseDir: 'dist'
-    }
+    files: [ 'dist/**/*.*' ],
+
+    // informs browser-sync to proxy our expressjs app which would run at the following location
+    proxy: 'http://localhost:3000',
+
+    // informs browser-sync to use the following port for the proxied app
+    // notice that the default port is 3000, which would clash with our expressjs
+    port: 4000,
   })
 })
 
-gulp.task('watch', [ 'browserSync', 'sass', 'html', 'js' ], function() {
-  gulp.watch('app/scss/**/*.scss', [ 'sass' ])
-  gulp.watch('app/index.html', [ 'html' ])
-  gulp.watch('app/js/**/*.js', [ 'js' ])
+gulp.task('default', [ 'browserSync', 'sass', 'html', 'js' ], function() {
+  gulp.watch('app/scss/**/*.scss', ['sass'])
+  gulp.watch('app/index.html', ['html'])
+  gulp.watch('app/js/**/*.js', ['js'])
 })
