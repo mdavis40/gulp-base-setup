c283d6314fb25916c5971e36aeae2675abe1e414
mdskd <mdavis0816@gmail.com>
2015-09-05 00:35:46 -0700

    favicon

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 var express = require('express'),
     app = express(),
-    path = require('path')
+    path = require('path'),
+    favicon = require('serve-favicon')
 
 app.get('/', function (request, response) {
   response.sendFile(path.join(__dirname + '/dist/index.html'))
 })
 
 app.use(express.static('dist'))
+app.use(favicon(__dirname + '/app/images/favicon.ico'))
 
 var server = app.listen(3000, function () {
   console.log('Running on port: ' + server.address().port)
