e4d64b99a4707c1dfb8d5eea56d748206e9433d0
mdskd <mdavis0816@gmail.com>
2015-09-05 21:42:23 -0700

    add a second card.

--- app/js/app.js
+++ app/js/app.js
@@ -6,12 +6,12 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
   $stateProvider
     .state('/', {
       url: '/',
-      template: '<md-card-content>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</md-card-content>',
+      template: '<md-card><md-card-content>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</md-card-content></md-card>',
       controller: 'AppCtrl'
     })
     .state('/content', {
       url: '/content',
-      template: '<md-card-content>{{name}}</md-card-content>',
+      template: '<md-card><md-card-content>{{name}}</md-card-content></md-card><md-card><md-card-content>{{place}}</md-card-content></md-card>',
       controller: 'AppCtrl'
     })
 }])
@@ -21,4 +21,5 @@ app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $u
   }
 
   $scope.name = 'Fred'
+  $scope.place ='Phx'
 }])
